10ede155c32ed99b5a811714fcd04b4e49856f60
Jonathan Edvard Bjerkedok <bjerkedok@gmail.com>
2025-07-13 16:52:29 +0200

    Moved transport into modbus.go to fit into AI module

--- modbus.go
+++ modbus.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+type transportType uint
+
+const (
+	modbusRTU        transportType = 1
+	modbusRTUOverTCP transportType = 2
+	modbusRTUOverUDP transportType = 3
+	modbusTCP        transportType = 4
+	modbusTCPOverTLS transportType = 5
+	modbusTCPOverUDP transportType = 6
+)
+
+type transport interface {
+	Close() error
+	ExecuteRequest(*pdu) (*pdu, error)
+	ReadRequest() (*pdu, error)
+	WriteResponse(*pdu) error
+}
+
 type pdu struct {
 	unitId       uint8
 	functionCode uint8
